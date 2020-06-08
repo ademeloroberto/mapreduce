d1c1830d75afd6a7f552f807f981487ffce28caf
ademeloroberto <analmroberto@gmail.com>
2020-06-07 21:22:59 -0300

    Exercise 6
    Average sales per category

--- reducer.py
+++ reducer.py
@@ -20,6 +20,7 @@ import sys
 # Cash  455.51
 
 # Sum of all sales (values) is initialized with zero, we just started
+sum_of_values = 0
 count_of_values = 0
 
 # Previous key is initialized with None, we just started
@@ -54,9 +55,10 @@ for line in sys.stdin:
     # the float function transforms the value
     # to a float data type (like decimal)
     count_of_values += 1
+    sum_of_values += float(value)
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
-if count_of values > 114:    
-    # write the last result to stdout
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+    
+# write the last result to stdout
+sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_of_values))
