90b7c6e7931164433181c3e41a60619432480d6e
ademeloroberto <analmroberto@gmail.com>
2020-06-07 21:10:44 -0300

    Exercise 3
    Raising an error

--- mapper.py
+++ mapper.py
@@ -36,9 +36,14 @@ for line in sys.stdin:
 
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
+    
+    if len(data) < 6:
+        raise ValueError("Not enough elements")
+        pass
+    else:
 
     # Write the key-value combination to standard output (stdout)
     # Key is the payment, value is the sales     
     # With a tab (\t) between key and value
     # New line \n means new record
-    sys.stdout.write("{0}\t{1}\n".format(category, sales))
+        sys.stdout.write("{0}\t{1}\n".format(category, sales))
